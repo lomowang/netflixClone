6972524faba9d8a1a55b3e53883a81ad1fd80226
lomowang <frees1102@gmail.com>
2023-09-17 17:34:06 +0800

    20230917

--- src/componet/HomeScreen.jsx
+++ src/componet/HomeScreen.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import requests from "@/Requests";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
     <div className="">
       <Nav />
       <Banner />
-
-      {/* Banner */}
-      {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Actio Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Documantaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
